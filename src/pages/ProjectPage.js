3fcab7e6d72fcb6a4293af553d7bf32e37ce3c6c
soundspeedskye <haneul.skye.cheon@gmail.com>
2024-09-09 18:37:04 +0900

    pdf, image error fix

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -8,15 +8,15 @@ const ProjectPage = ({ project }) => {
       case "project1":
         return (
           <NIMOPDFComponent
-            pdf_file1="/assets/pdf/nimonemo_P.pdf"
-            pdf_file2="/assets/pdf/nimonemo_T.pdf"
+            pdf_file1="assets/pdf/nimonemo_P.pdf"
+            pdf_file2="assets/pdf/nimonemo_T.pdf"
           />
         );
       case "project2":
         return (
           <DMAPDFComponent
-            pdf_file1="/assets/pdf/daengmyoapp_P.pdf"
-            pdf_file2="/assets/pdf/daengmyoapp_T.pdf"
+            pdf_file1="assets/pdf/daengmyoapp_P.pdf"
+            pdf_file2="assets/pdf/daengmyoapp_T.pdf"
           />
         );
       default:
