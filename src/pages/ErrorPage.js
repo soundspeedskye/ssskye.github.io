a590237dbaa7bb3cb4ae5d70ee775370e7a90d35
soundspeedskye <haneul.skye.cheon@gmail.com>
2024-09-09 23:49:15 +0900

    files update

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -3,7 +3,11 @@ import React from "react";
 const ErrorPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen ">
-      <img src="assets/images/ErrorImage.png" alt="Error" className="w-full" />
+      <img
+        src="./assets/images/ErrorImage.png"
+        alt="Error"
+        className="w-full"
+      />
     </div>
   );
 };
